d7e77b57a49ef92e6dbb4d4c2626d7ff63f1bb48
Thibaut Izard <thibaut.izard@gmail.com>
2024-11-29 23:40:44 +0100

    style: First layouts

---
+++ src/content/blog/second-article.md
@@ -0,0 +1,11 @@
+---
+title: 'Second post'
+description: 'Lorem ipsum dolor sit amet'
+author: 'Thibaut Izard'
+pubDate: 'Jul 22 2022'
+tags: ['js']
+---
+# Second note
+
+That's what I'm talking about bitch
+Who's the boss now ? 😈
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      // 🎨 Colors
+      colors: {
+        ...colors,
+        orange: '#F98513',
+        blue: '#223382',
+        lightBlue: '#9BACD8',
+        marine: '#111144',
+        beige: '#DAD1C8',
+        white: '#F4F1EC ',
+      },
+      // 🖋️ Fonts
+      fontFamily: {
+        rammetto: 'Rammetto One',
+        kanit: 'Kanit',
+        birthstone: 'Birthstone',
+      },
+    },
+  },
+  plugins: [],
+};
